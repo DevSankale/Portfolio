efe0062c75cd988c69072cf4621efa0833b311ac
DevSankale <sankaledev@gmail.com>
2025-01-10 14:47:43 +0300

    skills image icon update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,8 @@
 import styles from "./skills.module.css";
-import img1 from "./images/java-script.png";
-import img2 from "./images/react.png";
-import img3 from "./images/css.png";
-import img4 from "./images/html.png";
+import img1 from "./images/javascript.png";
+import img2 from "./images/reactimg.png";
+import img3 from "./images/cssimg.png";
+import img4 from "./images/htmlimg.png";
 import img5 from './images/tailwind.png'
 
 export default function Skills() {
@@ -10,26 +10,20 @@ export default function Skills() {
     <div className={styles.container}>
       <p className={styles.skill}>SKILLS.</p>
       <div className={styles.skills}>
-        
+      <div className={styles.div}>
+          <img src={img1} className={styles.skillimage} alt="JavaScript." />
+        </div>
         <div className={styles.div}>
           <img src={img2} className={styles.skillimage} alt="React." />
-          React.
         </div>
         <div className={styles.div}>
           <img src={img5} className={styles.skillimage} alt="Tailwind." />
-          Tailwind.
         </div>
         <div className={styles.div}>
           <img src={img3} className={styles.skillimage} alt="CSS." />
-          CSS.
         </div>
         <div className={styles.div}>
           <img src={img4} className={styles.skillimage} alt="HTML." />
-          Html.
-        </div>
-        <div className={styles.div}>
-          <img src={img1} className={styles.skillimage} alt="JavaScript." />
-          Javascript.
         </div>
       </div>
     </div>
