5af16141b0e5b8d86e1049a1b5ddcad70149115b
DevSankale <sankaledev@gmail.com>
2025-02-20 11:59:29 +0300

    nike project added

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,6 +1,14 @@
 export default [
   {
     id:1,
+    projectCover:"./projectImages/nike1.jpg",
+    title:"NikeCloneLandingPage",
+    description:"A responsive and visually striking landing page inspired by the Nike website. Built with React, Tailwind CSS, and Vite, this project showcases modern frontend development practices, including component-based architecture, fast build processes, and seamless styling. Integrated with Git for version control and hosted on GitHub Pages, it highlights proficiency in modern web development tools and deployment workflows.",
+    link:"https://devsankale.github.io/NikeCloneLandingPage/",
+    backgroundImage:"./images/nike1.jpg"
+  },
+  {
+    id:2,
     projectCover:"./projectImages/movie.jpg",
     title:"MovieApp",
     description:"MovieApp is a sleek and responsive movie discovery platform built with React and powered by The Movie Database (TMDB) API. It allows users to explore popular movies, search for specific titles, and view detailed movie information, including posters and overviews.The app features an intuitive search bar with real-time updates and a Favorites section for users to save movies they love. Optimized for both desktop and mobile, MovieApp ensures smooth navigation and performance. Built with Vite and modern React tools, it delivers a fast, user-friendly experience for movie enthusiasts."
@@ -9,7 +17,7 @@ export default [
     backgroundImage:"./images/movie.jpg"
   },
   {
-    id:2,
+    id:3,
     projectCover:"./projectImages/Recipe1.jpg",
     title:"RecipeApp",
     description:"A responsive web app that allows users to search for recipes based on ingredients or dish names, providing clear instructions and a list of ingredients. Designed with a clean UI and optimized for easy navigation on all device sizes."
@@ -19,14 +27,14 @@ export default [
 
 
   },{
-    id:3,
+    id:4,
     projectCover:"./projectImages/weather2.jpg",
     title:"WeatherApp",
     description:"An interactive React-based weather application that fetches real-time weather data from an external API. Users can search for current weather conditions by city and view details such as temperature, humidity, wind speed, and more. Designed with a responsive layout for a seamless experience across all devices.",
     link:"https://devsankale.github.io/WeatherApp/",
     backgroundImage:'./images/weather2.jpg',
   },{
-    id:4,
+    id:5,
     projectCover:"./projectImages/todo1.jpg",
     title:"TodoApp",
     description:" A user-friendly todo application built with React that allows users to create, complete, and delete tasks. The app features a clean interface with real-time updates on task completion status. Tasks are sorted based on their completion state, providing an organized view. It showcases effective state management and the ability to handle user interactions seamlessly."
@@ -37,7 +45,7 @@ export default [
 
   },
   {
-    id:5,
+    id:6,
     projectCover:"./projectImages/amazon2.jpg",
     title:"AmazonClone",
     description:" This project uses JavaScript, HTML, and CSS.It replicates the core functionalities of the popular e-commerce platform.This project offered a hands-on experience in building essential features of a modern e-commerce website."
@@ -47,7 +55,7 @@ export default [
   },
   
   {
-    id:6,
+    id:7,
     projectCover:"./projectImages/Capturesnip.JPG",
     title:"Landing Page.",
     description:"This Landing Page is a modern, responsive web page built using Tailwind CSS, a utility-first framework that allows for quick and efficient design customization. The page features a clean, minimalistic layout with utility classes for flexibility and responsiveness across devices. HTML and CSS are used for the structure and styling, while JavaScript enhances interactivity, especially for smooth navigation and dynamic content. The page also leverages responsive design principles, ensuring optimal viewing on various screen sizes. This landing page showcases the power of Tailwind CSS for building visually appealing and functional websites with ease and efficiency.",
@@ -55,20 +63,12 @@ export default [
     backgroundImage:"./images/Capturesnip.JPG"
   },
   {
-    id:7,
+    id:8,
     projectCover:"./projectImages/meme.jpg",
     title:"MemeGenerator.",
     description:"This projects is a simple version of a 'Meme-Generator App',build using Javascript,CSS and HTML as the core technologies.Where it generates different meme images when a user clicks a button.It utilizes React as the main technology behind the project.",
     link:"https://devsankale.github.io/MemeGenerator/",
     backgroundImage:"./images/meme.jpg"
   },
-    {
-      id:8,
-      projectCover:"./projectImages/rps1.jpg",
-      title:"Rock-Paper-Scissors",
-      description:"This projects replicates a simple version of a 'Rock Paper Scissors Game',build using Javascript,CSS and HTML as the core technologies.Where it compares the choices made between players and determines the winner.",
-      link:"https://devsankale.github.io/RPS-Game/",
-      backgroundImage:"./images/rps1.jpg"
-    },
 
   ]
